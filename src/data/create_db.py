8f00eb1fb9a91f7963fe69aa764fa7f090e4d8f4
luis.rachope <luis.rachopea@elgin.com.br>
2023-08-07 20:39:56 -0300

    Implementação da criação do banco quando não houver um criado

---
+++ src/data/create_db.py
@@ -0,0 +1,41 @@
+import sqlite3
+import os
+
+class Database:
+    def __init__(self):
+        self.conn = ''
+        self.database = 'src/data/db_generic.db'
+
+    def connect_db(self):
+        if os.path.exists(self.database):
+            self.conn = sqlite3.connect(self.database)
+            return self.conn
+        else:
+            self.conn = sqlite3.connect(self.database)
+            return None
+    
+    def create_table(self, table_name):
+        query = str(f'''CREATE TABLE {table_name}
+         (id INT PRIMARY KEY     NOT NULL,
+         name           TEXT    NOT NULL,
+         age            INT     NOT NULL,
+         email          CHAR(50));''')
+        
+        self.conn.execute(query)
+        self.conn.commit()
+
+    def insert_table(self, table_name):
+        self.conn.execute(f"INSERT INTO {table_name} (id,name,age,email) VALUES (1, 'Paul', 28, 'paul@email.com')")
+        self.conn.execute(f"INSERT INTO {table_name} (id,name,age,email) VALUES (2, 'Mark', 31, 'mark@email.com')")
+        self.conn.execute(f"INSERT INTO {table_name} (id,name,age,email) VALUES (3, 'Julia', 24, 'julia@email.com')")
+        self.conn.execute(f"INSERT INTO {table_name} (id,name,age,email) VALUES (4, 'Nathan', 48, 'nathan@email.com')")
+        self.conn.execute(f"INSERT INTO {table_name} (id,name,age,email) VALUES (5, 'Agatha', 19, 'agatha@email.com')")
+        self.conn.execute(f"INSERT INTO {table_name} (id,name,age,email) VALUES (6, 'Maxwell', 62, 'maxwell@email.com')")
+        self.conn.execute(f"INSERT INTO {table_name} (id,name,age,email) VALUES (7, 'Jonh', 31, 'jonh@email.com')")
+        self.conn.execute(f"INSERT INTO {table_name} (id,name,age,email) VALUES (8, 'Yennifer', 40, 'yennifer@email.com')")
+        self.conn.commit()
+
+    def select_table(self, table_name):
+        data = self.conn.execute(f"SELECT * FROM {table_name}")
+        print(data.fetchall())
+        return data
--- src/main.py
+++ src/main.py
@@ -1,9 +1,17 @@
 from flask import Flask
 from src.routes.user_routes import user as user_routes
+from src.data.create_db import Database
+
+dba = Database()
 
 def init_app():
     """Inicialização do core da Aplicação"""
     app = Flask(__name__, instance_relative_config=False)
+
+    if dba.connect_db() == None:
+        dba.create_table('user')
+        dba.insert_table('user')
+        dba.select_table('user')
 
     with app.app_context():
         app.register_blueprint(user_routes)
@@ -12,3 +20,5 @@ def init_app():
     
 app = init_app()
 #ctx = app.app_context()
+
+
